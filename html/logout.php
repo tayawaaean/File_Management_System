7fcb651cd63923230b08c6bd6f320cc8bf7ac05d
hevabii <165555394+hevabii@users.noreply.github.com>
2024-04-10 14:01:34 +0800

    21323

--- html/logout.php
+++ html/logout.php
@@ -1,13 +1,3 @@
-<?php
-session_start(); // Start the session
-
 // Unset all of the session variables
-$_SESSION = array();
-
-// Destroy the session
 session_destroy();
 
-// Redirect to login page
-header("Location: login.html");
-exit;
-?>
--- html/register.php
+++ html/register.php
@@ -1,23 +1,27 @@
 <?php
-	$name = $_POST['name'];
-	$email = $_POST['email'];
-	$username = $_POST['username'];
-	$password = $_POST['password'];
-	
-	// Database connection
-	$conn = new mysqli('localhost','root','','file_management_system_bingao');
-	if($conn->connect_error){
-		echo "$conn->connect_error";
-		die("Connection Failed : ". $conn->connect_error);
-	} else {
-		$stmt = $conn->prepare("insert into users(name, email, username, password) values(?, ?, ?, ?)");
-		$stmt->bind_param("ssss", $name, $email, $username, $password);
-		$execval = $stmt->execute();
-		echo $execval;
-		$stmt->close();
-		$conn->close();
-	}
+$name = $_POST['name'];
+$email = $_POST['email'];
+$username = $_POST['username'];
+$password = md5($_POST['password']); // Encrypting the password using MD5 hash
+
+// Database connection
+$conn = new mysqli('localhost', 'root', '', 'file_management_system_bingao');
+if ($conn->connect_error) {
+    echo "$conn->connect_error";
+    die("Connection Failed : " . $conn->connect_error);
+} else {
+    $stmt = $conn->prepare("INSERT INTO users (name, email, username, password) VALUES (?, ?, ?, ?)");
+    $stmt->bind_param("ssss", $name, $email, $username, $password);
+    $execval = $stmt->execute();
+    $stmt->close();
+    $conn->close();
+
+    // Redirect to index.html if account creation is successful
+    if ($execval) {
+        header("Location: login.php");
+        exit();
+    } else {
+        echo "Account creation failed!";
+    }
+}
 ?>
-
-
-
