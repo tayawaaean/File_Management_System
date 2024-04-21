cf1b6f8b758b5b892e95376463f3fed232cc0dd1
tayawaaean <143526706+tayawaaean@users.noreply.github.com>
2024-04-21 16:11:36 +0800

    Update personal_info.php

--- html/personal_info.php
+++ html/personal_info.php
@@ -13,7 +13,7 @@ include '../connection/verifier.php';?>
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
     <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200">
-    title>BNHS File Management System</title> 
+    <title>BNHS File Management System</title> 
 </head>
 <body>
     <nav>
